3d1d3f66a3a250ae1b10e84c578f6b644d383c49
cookpan001 <cookpan001@gmail.com>
2018-06-05 17:57:57 +0800

    add test Data usage

---
+++ test/app/Data/UserData.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Data;
+
+/**
+ * Description of UserData
+ *
+ * @author zhupeng <zhupeng@davdian.com>
+ */
+class UserData extends \Phoenix\Framework\Base\Database
+{
+    const DB_NAME = 'test';
+    const TABLE_NAME = 'user';
+}
